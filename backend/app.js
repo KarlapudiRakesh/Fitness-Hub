b4df82d2cec739a8fbbc25429ddcf419eef29c42
KarlapudiRakesh <160146429+KarlapudiRakesh@users.noreply.github.com>
2025-07-07 02:19:27 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -2,61 +2,52 @@ import express from "express";
 import { config } from "dotenv";
 import cors from "cors";
 import { sendEmail } from "./utils/sendEmail.js";
-const cors = require("cors");
 
-app.use(cors({
-  origin: process.env.FRONTEND_URL,
-  credentials: true,
-}));
 const app = express();
 const port = process.env.PORT || 4000;
-const router = express.Router();
 
 config({ path: "./config.env" });
 
-app.use(
-  cors({
-    origin: [process.env.FRONTEND_URL],
-    methods: ["POST"],
-    credentials: true,
-  })
-);
+app.use(cors({
+  origin: [process.env.FRONTEND_URL],
+  methods: ["POST"],
+  credentials: true,
+}));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-router.post("/send/mail", async (req, res, next) => {
+app.post("/send/mail", async (req, res) => {
   const { name, email, message } = req.body;
+
   if (!name || !email || !message) {
-    return next(
-      res.status(400).json({
-        success: false,
-        message: "Please provide all details",
-      })
-    );
+    return res.status(400).json({
+      success: false,
+      message: "Please provide all details",
+    });
   }
+
   try {
     await sendEmail({
-      email: "bet11.stake@gmail.com",
+      email: process.env.SMTP_MAIL,
       subject: "GYM WEBSITE CONTACT",
       message,
       userEmail: email,
     });
+
     res.status(200).json({
       success: true,
       message: "Message Sent Successfully.",
     });
   } catch (error) {
+    console.error("Error sending email:", error.message);
     res.status(500).json({
       success: false,
-      message: " Internal Server Error", 
+      message: "Internal Server Error",
     });
   }
 });
 
-app.use(router);
-
-app.listen(process.env.PORT, () => {
-  console.log(`Server listening at port ${process.env.PORT}`);
+app.listen(port, () => {
+  console.log(`Server listening at port ${port}`);
 });
-
